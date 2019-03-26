8811caf72b0573936a4de6d2a77f314359aa4b39
Vika Strigo <viktory120519841330@gmail.com>
2019-03-26 15:55:03 +0300

    Lesson 1, Lesson 2 : TaskA1, TaskA2, TaskA3, TaskB1, TaskB2, TaskB3, TaskC3

---
+++ src/by/it/vstrigo/TaskB1.java
@@ -0,0 +1,31 @@
+package by.it.vstrigo;
+
+/*
+Выводим квадрат числа
+Напишите программу, которая считывает с клавиатуры целое число a и выводит квадрат этого числа (a * a).
+Для считывания данных с клавиатуры используйте метод nextInt() объекта класса Scanner.
+
+Создать Scanner можно так:
+Scanner sc=new Scanner(System.in);
+
+Прочитать число можно так:
+int i=sc.nextInt();
+
+Требования:
+1. Программа должна выводить текст.
+2. В программе необходимо создать объект типа Scanner.
+3. Программа должна считывать число типа Int с клавиатуры.
+4. Программа должна выводить квадрат считанного числа.
+
+ */
+
+import java.util.Scanner;
+
+class TaskB1 {
+    public static void main(String[] args) {
+        System.out.println("Введите целое число и по окончании нажмите Enter");
+        Scanner scanner = new Scanner(System.in); // создание экземпляра обьекта класса Scsnner
+        int integerVar = scanner.nextInt(); //считывание вводимого с клавиатуры числа типа int в переменную integerVar
+        System.out.println((int)integerVar*integerVar); //вывод на консоль квадрата считанного числа
+    }
+}
