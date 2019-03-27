8e40526c44fd7913e0bcd27b63ba9fcc61776f26
Julia <juladv@mail.ru>
2019-03-27 23:41:47 +0300

    Tasks b

--- src/by/it/lapina/lesson02/TaskC1.java
+++ src/by/it/lapina/lesson02/TaskC1.java
@@ -10,8 +10,16 @@ package by.it.lapina.lesson02;
 Sum = 60
 
 */
-class TaskC1 {
+import java.util.Scanner;
 
+class TaskC1 {
+    public static void main(String[] args) {
+        Scanner g = new Scanner(System.in);
+        int num1 = g.nextInt();
+        int num2 = g.nextInt();
+        int num3 = num1+num2;
+        System.out.println("Sum = "+num3);
+    }
 
 
 
