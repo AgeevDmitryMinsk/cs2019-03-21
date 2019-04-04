7248725d10673d09024580360725f2205b3c5b56
BazekinNikita <voevodann@gmail.com>
2019-04-04 15:40:17 +0300

    lesson05 A1-A3 progress

--- src/by/it/davydovich/lesson05/TaskA3.java
+++ src/by/it/davydovich/lesson05/TaskA3.java
@@ -19,7 +19,7 @@ public class TaskA3 {
     public static void main(String[] args) {
         int [] arr = new int[10];
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < ; i++) {
+        for (int i = 0; i < 10; i++) {
             arr [i] = scanner.nextInt();
         }
         int min = arr [0];
