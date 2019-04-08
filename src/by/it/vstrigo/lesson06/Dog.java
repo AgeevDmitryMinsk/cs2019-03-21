e57b4e84e3a18d2783be2f3d4f51dc85a4856e00
Vika Strigo <viktory120519841330@gmail.com>
2019-04-08 15:12:04 +0300

    Lesson 06

---
+++ src/by/it/vstrigo/lesson06/Dog.java
@@ -0,0 +1,29 @@
+package by.it.vstrigo.lesson06;
+
+public class Dog {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return ("Кличка: " + name + ". Возраст: " + age);
+    }
+
+
+}
