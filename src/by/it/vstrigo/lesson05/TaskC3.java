2605b025927c0eca94b45b151dc8f8eb0cb356bd
Vika Strigo <viktory120519841330@gmail.com>
2019-04-09 12:04:48 +0300

    Lesson 05, TaskC03 обновленный

--- src/by/it/vstrigo/lesson05/TaskC3.java
+++ src/by/it/vstrigo/lesson05/TaskC3.java
@@ -9,7 +9,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class TaskC3 {
-    //используется сортировка массива методом слияния Merge Sort
+    static int mergeN =0;
+    static int devideN =0;
     public static void main(String[] args) {
         int[] arr = new int[20];
         Scanner sc = new Scanner(System.in);
@@ -19,39 +20,46 @@ public class TaskC3 {
         for (int i = 0; i < array.length; i++)
             System.out.println(array[i]);
     }
-    //рекурсивная функция деления массива надвое, в которую вложенна функция слияния
-    public static int[] sort(int[] arr) {
-        if (arr.length < 2) return arr;//условие прекращения рекурсии
-        int m = arr.length / 2;//метка границы подмассивов
-        int[] arr1 = Arrays.copyOfRange(arr, 0, m);//левая часть массива, не включающая рассчитанную метку,
-        // копируется в первый подмассив
-        int[] arr2 = Arrays.copyOfRange(arr, m, arr.length);//левая часть массива, включающая рассчитанную метку,
-        // копируется во второй подмассив
-        return merge(sort(arr1), sort(arr2));//рекурсивный вызов функции деления подмассивов с последующей
-        // передачей полученных минимальных подмассивов в качестве аргументов методу слияния
+
+    public static int[] sort(int[] arr) {// сортировка MergeSort
+        if (arr.length < 2) return arr;
+        int m = arr.length / 2;
+        devideN++;
+        System.out.println("Вызов деления массива №" + devideN + " метка разделителя m = " + m);
+        int[] arr1 = Arrays.copyOfRange(arr, 0, m);
+        int[] arr2 = Arrays.copyOfRange(arr, m, arr.length);
+        int[] left = sort(arr1);
+        System.out.println("Передача первого (левого) аргумента методу слияния");
+        int[] right = sort(arr2);
+        System.out.println("Передача второго (правого) аргумента методу слияния");
+        return merge(left, right);
     }
-    //слияние двух массивов в один отсортированный
-    public static int[] merge(int[] arr1, int arr2[]) {
-        int n = arr1.length + arr2.length;
+
+    public static int[] merge(int[] left, int [] right) {
+        mergeN++;
+        System.out.println("Вызов слияния массива №" + mergeN);
+        int n = left.length + right.length;
         int[] buf = new int[n];
-        int i1 = 0;//указатель наибольшего элемента в левой части массива
-        int i2 = 0;//указатель наибольшего элемента в правой части массива
+        int iL = 0;
+        int iR = 0;
         for (int i = 0; i < n; i++) {
-            if (i1 == arr1.length) {//если элементы в левой части массива закончились - левый указатель вышел за пределы
-                // сортируемого участка
-                buf[i] = arr2[i2];
-                i2++;
-            } else if (i2 == arr2.length) {//если элементы в правой части массива закончились - правый указатель вышел
-                // за пределы сортируемого участка
-                buf[i] = arr1[i1];
-                i1++;
+            if (iL == left.length) {
+                buf[i] = right[iR];
+                iR++;
+                System.out.println("buf[" + i + "]=" + buf[i]);
+            } else if (iR == right.length) {
+                buf[i] = left[iL];
+                iL++;
+                System.out.println("buf[" + i + "]=" + buf[i]);
             } else {
-                if (arr1[i1] > arr2[i2]) {
-                    buf[i] = arr1[i1];
-                    i1++;
+                if (left[iL] > right[iR]) {
+                    buf[i] = left[iL];
+                    iL++;
+                    System.out.println("buf[" + i + "]=" + buf[i]);
                 } else {
-                    buf[i] = arr2[i2];
-                    i2++;
+                    buf[i] = right[iR];
+                    iR++;
+                    System.out.println("buf[" + i + "]=" + buf[i]);
                 }
             }
         }
